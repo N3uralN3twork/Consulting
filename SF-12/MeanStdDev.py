824f66c6740e16b00a73a738a68b4ff45004331b
N3uralN3twork <miq_qedquinn@yahoo.com>
2020-08-27 15:08:18 -0400

    SF-12

--- SF-12/MeanStdDev.py
+++ SF-12/MeanStdDev.py
@@ -1,18 +1,20 @@
 """
-Title: Step One of Scoring the SF-12
+Title: Step One of Scoring the SF-12 version 2
 Author: Matt Quinn
 Date: 25th August 2020
 Class: STA 635
+Resources:
+
 """
 ####################################################################
-###            Set the WD and Import the Libraries               ###
+#                Set the WD and Import the Libraries               #
 ####################################################################
 import os
-os.chdir(path="C:/Users/miqui/OneDrive/Consulting/SF-12")
+os.chdir(path="C:/Users/miqui/OneDrive/CSU Classes/Consulting/SF-12")
 os.listdir()
 
-import numpy as np
 import pandas as pd
+import numpy as np
 
 pd.set_option('display.max_columns', 10)
 
@@ -26,4 +28,40 @@ nomiss = pd.read_excel("SF12_nomissing.xlsx",
                      sheet_name="data")
 nomiss.head(3)
 nomiss.describe()
+
+####################################################################
+#           Step 1: Code Out-of-Range Values to NA                 #
+####################################################################
+
+nomiss.columns
+
+"Ranges [1 - 3]:"
+OnetoThreeColumns = ["Y2", "Y3"]
+
+# Change out of range values to NA for questions with a range between 1 and 3:
+nomiss[OnetoThreeColumns] = nomiss[OnetoThreeColumns].applymap(lambda x: np.where(x in range(1, 4), x, None))
+
+"Ranges [1 - 5]:"
+OnetoFiveColumns = ["Y1", "Y4", "Y5", "Y6", "Y7",
+                    "Y8", "Y9", "Y10", "Y11", "Y12"]
+
+# Change out of range values to NA for questions with a range between 1 and 5:
+nomiss[OnetoFiveColumns] = nomiss[OnetoFiveColumns].applymap(lambda x: np.where(x in range(1, 6), x, None))
+
+"Check our results: "
+nomiss.describe()
+
+####################################################################
+#         Step 2: Reverse Code 4 Items to Higher Scale             #
+####################################################################
+
+"General Formula for reversing a scale = (N+1)-i:"
+
+ReverseVars = ["Y1", "Y8", "Y9", "Y10"]
+
+df = nomiss.copy()
+
+nomiss[ReverseVars] = nomiss[ReverseVars].applymap(lambda x: 5-x)
+
+
 
--- SF-12/SF-12 Code in R.R
+++ SF-12/SF-12 Code in R.R
@@ -1,6 +1,107 @@
-setwd("C:/Users/miqui/OneDrive/Consulting/SF-12")
+setwd("C:/Users/miqui/OneDrive/CSU Classes/Consulting/SF-12")
 library(MLCIRTwithin)
-data("SF12")
-data("SF12_nomiss")
+library(readr)
+library(dplyr)
+#data("SF12")
+#data("SF12_nomiss")
 
-summary(SF12)
+'summary(SF12)
+
+table(sf12$V1)
+table(SF12$Y11)
+table(SF12$Y12)'
+
+"No missing dataset: "
+library(readxl)
+nomiss <- read_excel("SF12_nomissing.xlsx")
+View(nomiss)
+
+"Whole dataset: "
+sf12 <- read_csv("SF-12.csv")
+View(sf12)
+attach(sf12)
+
+"add 1 to the 12  columns and then check for out of range values
+Then reverse the last two variables"
+
+names(sf12)
+
+"Individual variable frequencies: "
+# In general, would you say your health is... :
+table(H0015900)
+
+# Health limits in moderate activities ... :
+table(H0016000)
+
+# Health limits climbing several flights of stairs ... :
+table(H0016100)
+
+# Accomplished less than you would like / physical health ... :
+table(H0016200)
+
+# Limited in the kind of work or daily activities ... :
+table(H0016300)
+
+# Accomplished less than you would like / emotional health ... :
+table(H0016400)
+
+# Did work less carefully / emotional health ... :
+table(H0016500)
+
+# How much did pain interfere with your normal work :
+table(H0016600)
+
+# How much of the time have you felt calm and peaceful : 
+table(H0016700)
+
+# How much of the time did you have a lot of energy :
+table(H0016800)
+
+# How much of the time did you feel downhearted and blue :
+table(H0016900)
+
+# Health interfered with your social activities :
+table(H0017000)
+
+
+"The codebook provided for dataset 3 matches up with data3!"
+
+"""
+-1 = Refusal
+-2 = Don't Know
+-4 = Legitamite Skip
+-5 = Non-interview
+"""
+
+sf12 <- sf12 %>%
+  mutate(GeneralHealth = replace(H0015900, H0015900 %in% c(-1, -2, -4, -5), NA)) %>%
+  mutate(ModerateActivities = replace(H0016000, H0016000 %in% c(-1, -2, -4, -5), NA)) %>%
+  mutate(Stairs = replace(H0016100, H0016100 %in% c(-1, -2, -4, -5), NA)) %>%
+  mutate(AccomplishLessPhysical = replace(H0016200, H0016200 %in% c(-1, -2, -4, -5), NA)) %>%
+  mutate(LimitedWork = replace(H0016300, H0016300 %in% c(-1, -2, -4, -5), NA)) %>%
+  mutate(AccomplishLessEmotional = replace(H0016400, H0016400 %in% c(-1, -2, -4, -5), NA)) %>%
+  mutate(WorkLessCareful = replace(H0016500, H0016500 %in% c(-1, -2, -4, -5), NA)) %>%
+  mutate(PainInterfere = replace(H0016600, H0016600 %in% c(-1, -2, -4, -5), NA)) %>%
+  mutate(CalmPeaceful = replace(H0016700, H0016700 %in% c(-1, -2, -4, -5), NA)) %>%
+  mutate(LotsEnergy = replace(H0016800, H0016800 %in% c(-1, -2, -4, -5), NA)) %>%
+  mutate(FeelBlue = replace(H0016900, H0016900 %in% c(-1, -2, -4, -5), NA)) %>%
+  mutate(SocialActivities = replace(H0017000, H0017000 %in% c(-1, -2, -4, -5), NA))
+
+attach(sf12)
+table(GeneralHealth)
+table(ModerateActivities)
+table(Stairs)
+table(AccomplishLessPhysical)
+table(LimitedWork)
+table(AccomplishLessEmotional)
+table(WorkLessCareful)
+table(PainInterfere)
+table(CalmPeaceful)
+table(LotsEnergy)
+table(FeelBlue)
+table(SocialActivities)
+
+
+
+
+
