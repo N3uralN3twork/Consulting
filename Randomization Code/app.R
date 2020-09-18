fb014509591adae8b95c6b25b0305b12177077cc
N3uralN3twork <miq_qedquinn@yahoo.com>
2020-09-17 22:59:07 -0400

    Update app.R

--- Randomization Code/app.R
+++ Randomization Code/app.R
@@ -187,12 +187,12 @@ schema <- function(Sites = NULL, NSubjects, BlockSize = NULL, RRatio = NULL, see
   }
   
   # Adding the Ts and Cs:
-  blocks = block.random(n = NSubjects, ncond = BlockSize)
-  Ts = RRatio/(RRatio+1)
-  Cs = 1-Ts
-  TorC = block_ra(blocks = blocks[ , 1],
-                  conditions = c("T", "C"),
-                  prob_each = c(Ts, Cs))
+  blocks = block.random(n = NSubjects, ncond = BlockSize) # Create blocks first
+  Ts = RRatio/(RRatio+1) # Compute the number of Ts needed
+  Cs = 1-Ts # Compute the number of Cs needed
+  TorC = block_ra(blocks = blocks[ , 1], # Using the first column:
+                  conditions = c("T", "C"), # 2 groups are the Ts and Cs
+                  prob_each = c(Ts, Cs)) # The prob. that T or C appears
   TLC = data.frame(TorC)
   
   # Shuffle the data randomly:
