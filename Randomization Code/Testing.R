0068a98d170540ffb66b6bfcc1f60736d1ef03c5
N3uralN3twork <miq_qedquinn@yahoo.com>
2020-11-11 20:35:08 -0500

    Update Testing.R

--- Randomization Code/Testing.R
+++ Randomization Code/Testing.R
@@ -1,9 +1,33 @@
-library(MLeval)
+library(gee)
+
+names(loans)
+table(loans$DelinquentBinary)
+
+loans$FICO_Category <- factor(loans$FICO_Category, exclude = NaN)
+
+gee1 <- gee(DelinquentBinary ~ FICO_Category + FICO_Score,
+            data = loans,
+            family = binomial(link = "logit"),
+            corstr = "exchangeable",
+            id = Account_Number)
+
+summary(gee1)
+
+# Get Odds coefficients:
+
+round(exp(coef(gee1)), 5)
+
+"Compared to customers with very low credit history, the odds of being delinquent
+are 4.11 times lower for those with very good credit history."
+
+"Both SAS and R provide the same conclusion and numbers."
+
+a = parameterEstimates(fit4, level = 0.95)$lhs
+b = parameterEstimates(fit4, level = 0.95)$op
+c = parameterEstimates(fit4, level = 0.95)$rhs
+d = parameterEstimates(fit4, level = 0.95)$ci.lower
+e = parameterEstimates(fit4, level = 0.95)$ci.upper
 
 
-
-res <- evalm(glm, gnames = c("glm"))
-
-resamps <- resamples(list(GLM = glm,
-                          RF = rf,
-                          ))
+result = cbind(a, b, c, exp(d), exp(e))
+result
